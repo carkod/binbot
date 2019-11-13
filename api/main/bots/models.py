90d2c7739a366bbab044776c894ae450af6c8c1e
Carlos Wu <carkodw@gmail.com>
2019-11-13 20:09:24 +0000

    static vars bot creation

--- api/main/bots/models.py
+++ api/main/bots/models.py
@@ -23,7 +23,7 @@ class Bot(Account, Ticker24Data):
 
     def __init__(self):
         self.defaults = {
-            "pairs": "",
+            "pair": "",
             "active": False,
             "strategy": 'long',
             "name": 'Default Bot',
@@ -40,16 +40,24 @@ class Bot(Account, Ticker24Data):
             "trailling_deviation": 0.0063,
             "deal_min_value": 0,
             "cooldown": 0,
-            "active": False
         }
+        self.balance_division = 0
 
-    def get_base_order_size(self, take_profit=0.03):
-        df = Ticker24Data(app).api_data()
-        #  * self.balance_division
-        return 0.0001
+    # def get_base_order_size(self, pair, balance_usage=1, max_so_count=5):
+    #     df = Ticker24Data(app).api_data()
+    #     available_balance = balance_usage * Account().get_balances()
+    #     self.balance_division = available_balance / max_so_count
+    #     price = Ticker24Data(pair).request_data()
+    #     base_order_size = self.balance_division / price
+    #     return base_order_size
 
-    def get_so_size(self):
-        return 0.0001
+    # def get_so_size(self, base_order_size, index, so_size):
+    #     i = index + 1
+    #     size = base_order_size + self.balance_division
+    #     so_size
+    #     self.get_base_order_size()
+        
+    #     return so_size
 
     def get_start_condition(self):
         return True
@@ -61,32 +69,30 @@ class Bot(Account, Ticker24Data):
             resp = tools.JsonResp({ "data": bot }, 200)
         else:
             resp = tools.JsonResp({ "message": "Bots not found" }, 404)
-
         return resp
 
     def create(self):
         resp = tools.JsonResp({ "message": "Bot creation not available" }, 400)
         data = json.loads(request.data)
-
+        # base_order_size = self.get_base_order_size(data['pair'], data['maxSOCount'], data['take_profit'])
         new_bot = {
-            "pairs": data['pairs'],
+            "pair": data['pair'],
             "active": data['active'] or False,
             "strategy": data['strategy'] or 'long',
             "name": data['name'] or 'Default Bot',
             "max_so_count": data['maxSOCount'] or 3,
             "balance_usage": data['balanceUsage'], # 100% of All Btc balance
             "balance_usage_size": data['balanceUsage'] * Account().get_balances(),
-            "base_order_size": self.get_base_order_size(data['take_profit']) or 0.003, # MIN by Binance = 0.0001 BTC
-            "base_order_type": data['baseOrderType'],
-            "start_condition": self.get_start_condition() or True,
-            "so_size": self.get_so_size(), # Top band 
+            "base_order_size": data['baseOrderSize'] or 0.0001, # MIN by Binance = 0.0001 BTC
+            "base_order_type": data['baseOrderType'], # Market or limit
+            "start_condition": True,
+            "so_size": data['soSize'] or 0.0001, # Top band 
             "take_profit": data['takeProfit'] or 0.003,
             "price_deviation_so": data['priceDeviationSO'] or 0.0063, # % percentage
             "trailling": data['trailling'] or False,
             "trailling_deviation": data['trailling_deviation'] or 0.0063,
             "deal_min_value": data['dealMinValue'] or 0,
             "cooldown": data['cooldown'] or 0,
-            "active": data['active'] or False
         }
         self.defaults.update(new_bot)
         botId = app.db.bots.save(new_bot)
@@ -103,20 +109,26 @@ class Bot(Account, Ticker24Data):
         data = json.loads(request.data)
 
         existent_bot = {
-            "pairs": data['pairs'],
-            "name": data['name'],
-            "bot_type": data['botType'],
-            "base_order_size": data['baseOrderSize'], # MIN by Binance = 0.0001 BTC
-            "base_order_type": data['baseOrderType'],
-            "start_condition": data['startCondition'],
-            "max_so_count": data['maxSOCount'],
-            "so_size": self.so_size, # Top band 
-            "price_deviation_so": 0.63, # % percentage
-            "deal_min_value": self.deal_min_value,
-            "cooldown": self.cooldown,
-            "active": data['active']
+            "pair": data['pair'],
+            "active": data['active'] or False,
+            "strategy": data['strategy'] or 'long',
+            "name": data['name'] or 'Default Bot',
+            "max_so_count": data['maxSOCount'] or 3,
+            "balance_usage": data['balanceUsage'], # 100% of All Btc balance
+            "balance_usage_size": data['balanceUsage'] * Account().get_balances(),
+            "base_order_size": data['base_order_size'] or 0.0001, # MIN by Binance = 0.0001 BTC
+            "base_order_type": data['baseOrderType'], # Market or limit
+            "start_condition": True,
+            "so_size": data['soSize'] or 0.0001, # Top band 
+            "take_profit": data['takeProfit'] or 0.003,
+            "price_deviation_so": data['priceDeviationSO'] or 0.0063, # % percentage
+            "trailling": data['trailling'] or False,
+            "trailling_deviation": data['trailling_deviation'] or 0.0063,
+            "deal_min_value": data['dealMinValue'] or 0,
+            "cooldown": data['cooldown'] or 0,
         }
-        
+
+        self.defaults.update(existent_bot)
         botId = app.db.bots.update_one({ "_id": data["_id"] }, { "$set": existent_bot }, upsert=False)
         if (botId.acknowledged):
             resp = tools.JsonResp({"message": "Successfully updated bot", "botId": data["_id"]}, 200)
