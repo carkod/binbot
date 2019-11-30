bea20de44178b86186cec61c01f7da4da1699a7c
Carlos Wu <carkodw@gmail.com>
2019-11-30 18:41:51 +0000

    black and safety orders

--- api/main/bots/models.py
+++ api/main/bots/models.py
@@ -22,36 +22,36 @@ from bson.objectid import ObjectId
 
 
 class Bot:
-
     def __init__(self):
         self.defaults = {
             "pair": "",
             "active": False,
-            "strategy": 'long',
-            "name": 'Default Bot',
+            "strategy": "long",
+            "name": "Default Bot",
             "max_so_count": 3,
-            "balance_usage": float(1), # 100% of All Btc balance
+            "balance_usage": float(1),  # 100% of All Btc balance
             "balance_usage_size": 0.0001,
-            "base_order_size": 0.003, # MIN by Binance = 0.0001 BTC
-            "base_order_type": 'limit',
+            "base_order_size": 0.003,  # MIN by Binance = 0.0001 BTC
+            "base_order_type": "limit",
             "start_condition": True,
-            "so_size": 0.0001, # Top band 
+            "so_size": 0.0001,  # Top band
             "take_profit": 0.003,
-            "price_deviation_so": 0.0063, # % percentage
+            "price_deviation_so": 0.0063,  # % percentage
             "trailling": False,
             "trailling_deviation": 0.0063,
             "deal_min_value": 0,
             "cooldown": 0,
+            "deals": []
         }
         self.balance_division = 0
         self.active_bot = None
 
     def get_available_btc(self):
-        data = json.loads(Account().get_balances().data)['data']
+        data = json.loads(Account().get_balances().data)["data"]
         available_balance = 0
         for i in range(len(data)):
-            if data[i]['asset'] == 'BTC':
-                available_balance = data[i]['free']
+            if data[i]["asset"] == "BTC":
+                available_balance = data[i]["free"]
                 return available_balance
         return available_balance
 
@@ -59,133 +59,149 @@ class Bot:
         return True
 
     def get(self):
-        resp = tools.JsonResp({ "message": "No bots found" }, 200)
+        resp = tools.JsonResp({"message": "No bots found"}, 200)
         bot = list(app.db.bots.find())
         if bot:
-            resp = tools.JsonResp({ "data": bot }, 200)
+            resp = tools.JsonResp({"data": bot}, 200)
         else:
-            resp = tools.JsonResp({ "message": "Bots not found" }, 404)
+            resp = tools.JsonResp({"message": "Bots not found"}, 404)
         return resp
-    
+
     def get_one(self):
-        resp = tools.JsonResp({ "message": "No bots found" }, 200)
-        findId = request.view_args['id']
-        bot = list(app.db.bots.find_one({ "_id": findId }))
-        
+        resp = tools.JsonResp({"message": "No bots found"}, 200)
+        findId = request.view_args["id"]
+        bot = list(app.db.bots.find_one({"_id": findId}))
+
         if bot:
-            resp = tools.JsonResp({ "data": bot }, 200)
+            resp = tools.JsonResp({"data": bot}, 200)
         else:
-            resp = tools.JsonResp({ "message": "Bots not found" }, 404)
+            resp = tools.JsonResp({"message": "Bots not found"}, 404)
         return resp
 
     def create(self):
-        resp = tools.JsonResp({ "message": "Bot creation not available" }, 400)
+        resp = tools.JsonResp({"message": "Bot creation not available"}, 400)
         data = json.loads(request.data)
         btc_balance = self.get_available_btc()
         # base_order_size = self.get_base_order_size(data['pair'], data['maxSOCount'], data['take_profit'])
         new_bot = {
-            "pair": data['pair'],
-            "active": data['active'] or False,
-            "strategy": data['strategy'] or 'long',
-            "name": data['name'] or 'Default Bot',
-            "max_so_count": data['maxSOCount'] or 3,
-            "balance_usage": data['balanceUsage'], # 100% of All Btc balance
-            "balance_usage_size": float(data['balanceUsage']) * btc_balance,
-            "base_order_size": data['baseOrderSize'] or 0.0001, # MIN by Binance = 0.0001 BTC
-            "base_order_type": data['baseOrderType'], # Market or limit
+            "pair": data["pair"],
+            "active": data["active"] or False,
+            "strategy": data["strategy"] or "long",
+            "name": data["name"] or "Default Bot",
+            "max_so_count": data["maxSOCount"] or 3,
+            "balance_usage": data["balanceUsage"],  # 100% of All Btc balance
+            "balance_usage_size": float(data["balanceUsage"]) * btc_balance,
+            "base_order_size": data["baseOrderSize"] or 0.0001,  # MIN by Binance = 0.0001 BTC
+            "base_order_type": data["baseOrderType"],  # Market or limit
             "start_condition": True,
-            "so_size": data['soSize'] or 0.0001, # Top band 
-            "take_profit": data['takeProfit'] or 0.003,
-            "price_deviation_so": data['priceDeviationSO'] or 0.0063, # % percentage
-            "trailling": data['trailling'] or False,
-            "trailling_deviation": data['traillingDeviation'] or 0.0063,
-            "deal_min_value": data['dealMinValue'] or 0,
-            "cooldown": data['cooldown'] or 0,
+            "so_size": data["soSize"] or 0.0001,  # Top band
+            "take_profit": data["takeProfit"] or 0.003,
+            "price_deviation_so": data["priceDeviationSO"] or 0.0063,  # % percentage
+            "trailling": data["trailling"] or False,
+            "trailling_deviation": data["traillingDeviation"] or 0.0063,
+            "deal_min_value": data["dealMinValue"] or 0,
+            "cooldown": data["cooldown"] or 0,
         }
         self.defaults.update(new_bot)
         botId = app.db.bots.save(new_bot)
-        if (botId):
-            resp = tools.JsonResp({ "message": "Successfully created new bot", "botId": str(botId)}, 200)
+        if botId:
+            resp = tools.JsonResp(
+                {"message": "Successfully created new bot", "botId": str(botId)}, 200
+            )
         else:
-            resp = tools.JsonResp({ "message": "Failed to create new bot" }, 400)
-            
+            resp = tools.JsonResp({"message": "Failed to create new bot"}, 400)
 
         return resp
 
     def edit(self):
-        resp = tools.JsonResp({ "message": "Bot update is not available" }, 400)
+        resp = tools.JsonResp({"message": "Bot update is not available"}, 400)
         data = json.loads(request.data)
 
         existent_bot = {
-            "pair": data['pair'],
-            "active": data['active'] or False,
-            "strategy": data['strategy'] if data.get('strategy') else 'long',
-            "name": data['name'] if data.get('name') else 'Default Bot',
-            "max_so_count": data['maxSOCount'] if data.get('maxSOCount') else 3,
-            "balance_usage": data['balanceUsage'] if data.get('balanceUsage') else 1, # 100% of All Btc balance
-            "base_order_size": data['baseOrderSize'], # MIN by Binance = 0.0001 BTC
-            "base_order_type": data['baseOrderType'] if data.get('baseOrderType') else 'limit', # Market or limit
+            "pair": data["pair"],
+            "active": data["active"] or False,
+            "strategy": data["strategy"] if data.get("strategy") else "long",
+            "name": data["name"] if data.get("name") else "Default Bot",
+            "max_so_count": data["maxSOCount"] if data.get("maxSOCount") else 3,
+            "balance_usage": data["balanceUsage"]
+            if data.get("balanceUsage")
+            else 1,  # 100% of All Btc balance
+            "base_order_size": data["baseOrderSize"],  # MIN by Binance = 0.0001 BTC
+            "base_order_type": data["baseOrderType"]
+            if data.get("baseOrderType")
+            else "limit",  # Market or limit
             "start_condition": True,
-            "so_size": data['soSize'], # Top band 
-            "take_profit": data['takeProfit'],
-            "price_deviation_so": data['priceDeviationSO'], # % percentage
-            "trailling": data['trailling'],
-            "trailling_deviation": data['traillingDeviation'],
-            "deal_min_value": data['dealMinValue'],
-            "cooldown": data['cooldown'],
+            "so_size": data["soSize"],  # Top band
+            "take_profit": data["takeProfit"],
+            "price_deviation_so": data["priceDeviationSO"],  # % percentage
+            "trailling": data["trailling"],
+            "trailling_deviation": data["traillingDeviation"],
+            "deal_min_value": data["dealMinValue"],
+            "cooldown": data["cooldown"],
         }
 
         self.defaults.update(existent_bot)
-        botId = app.db.bots.update_one({ "_id": data["_id"] }, { "$set": existent_bot }, upsert=False)
-        if (botId.acknowledged):
-            resp = tools.JsonResp({"message": "Successfully updated bot", "botId": data["_id"]}, 200)
+        botId = app.db.bots.update_one({"_id": data["_id"]}, {"$set": existent_bot}, upsert=False)
+        if botId.acknowledged:
+            resp = tools.JsonResp(
+                {"message": "Successfully updated bot", "botId": data["_id"]}, 200
+            )
         else:
-            resp = tools.JsonResp({ "message": "Failed to update bot" }, 400)
-        
+            resp = tools.JsonResp({"message": "Failed to update bot"}, 400)
+
         return resp
 
     def required_field_validation(self, data, key):
         if key in data:
             return data[key]
         else:
-            resp = tools.JsonResp({"message": "Validation failed {} is required".format(key), "botId": data
-            ["_id"]}, 400)
+            resp = tools.JsonResp(
+                {"message": "Validation failed {} is required".format(key), "botId": data["_id"]},
+                400,
+            )
             return resp
 
     def delete(self, id):
-        resp = tools.JsonResp({ "message": "Bot update is not available" }, 400)
-        id = request.view_args['id']
-        delete_action = app.db.bots.delete_one({ "_id": ObjectId(id) })
-        if (delete_action):
-            resp = tools.JsonResp({ "message": "Successfully delete bot", "botId": id }, 200)
+        resp = tools.JsonResp({"message": "Bot update is not available"}, 400)
+        id = request.view_args["id"]
+        delete_action = app.db.bots.delete_one({"_id": ObjectId(id)})
+        if delete_action:
+            resp = tools.JsonResp({"message": "Successfully delete bot", "botId": id}, 200)
         else:
-            resp = tools.JsonResp({ "message": "Bot deletion is not available" }, 400)
+            resp = tools.JsonResp({"message": "Bot deletion is not available"}, 400)
         return resp
 
-
     def activate(self):
-        resp = tools.JsonResp({ "message": "Bot activation is not available" }, 400)
-        findId = request.view_args['botId']
-        bot = app.db.bots.find_one({ "_id": ObjectId(findId) })
-        if (bot):
-            bot['active'] = True
-            app.db.bots.save(bot)
-            if Deal(bot).open_deal():
-                resp = tools.JsonResp({ "message": "Successfully activated bot", "bodId": str(findId) }, 200)
+        resp = tools.JsonResp({"message": "Bot activation is not available"}, 400)
+        findId = request.view_args["botId"]
+        bot = app.db.bots.find_one({"_id": ObjectId(findId)})
+        if bot:
+            bot["active"] = True
+            dealId = Deal(bot, app).open_deal()
+            if dealId:
+                if 'deals' not in bot.keys():
+                    bot["deals"] = []
+                bot["deals"].append(dealId)
+                app.db.bots.save(bot)
+                resp = tools.JsonResp(
+                    {"message": "Successfully activated bot", "bodId": str(findId)}, 200
+                )
+                return resp
             else:
-                resp = tools.JsonResp({ "message": "Deal creation failed", "botId": findId }, 400)    
+                resp = tools.JsonResp({"message": "Deal creation failed", "botId": findId}, 400)
         else:
-            resp = tools.JsonResp({ "message": "Bot not found", "botId": findId }, 400)
+            resp = tools.JsonResp({"message": "Bot not found", "botId": findId}, 400)
         return resp
 
     def deactivate(self):
-        resp = tools.JsonResp({ "message": "Bot deactivation is not available" }, 400)
-        findId = request.view_args['botId']
-        bot = app.db.bots.find_one({ "_id": ObjectId(findId) })
-        if (bot):
+        resp = tools.JsonResp({"message": "Bot deactivation is not available"}, 400)
+        findId = request.view_args["botId"]
+        bot = app.db.bots.find_one({"_id": ObjectId(findId)})
+        if bot:
             bot.active = False
             # Deal(bot).open_deal()
-            resp = tools.JsonResp({ "message": "Successfully deactivated bot", "data": bot }, 200)
+            resp = tools.JsonResp({"message": "Successfully deactivated bot", "data": bot}, 200)
         else:
-            resp = tools.JsonResp({ "message": "Bot not found", "botId": findId }, 400)
+            resp = tools.JsonResp({"message": "Bot not found", "botId": findId}, 400)
         return resp
+
--- api/main/deals/models.py
+++ api/main/deals/models.py
@@ -21,9 +21,10 @@ from main.orders.models import Buy_Order, Sell_Order
 
 load_dotenv()
 
-class Deal:
 
-    def __init__(self, bot):
+class Deal:
+    def __init__(self, bot, app):
+        self.app = app
         self.key = os.getenv("BINANCE_KEY")
         self.secret = os.getenv("BINANCE_SECRET")
         self.base_url = os.getenv("BASE")
@@ -32,20 +33,20 @@ class Deal:
         # Buy order
         self.side = EnumDefinitions.order_side[0]
 
-        self.symbol = bot['pair']
-        self.botname = bot['name']
-        self.active = bot['active']
-        self.balance = bot['balance_usage_size']
-        self.base_order_type = bot['base_order_type']
-        self.max_so_count = int(bot['max_so_count'])
-        self.price_deviation_so = bot['price_deviation_so']
+        self.symbol = bot["pair"]
+        self.botname = bot["name"]
+        self.active = bot["active"]
+        self.balance = bot["balance_usage_size"]
+        self.base_order_type = bot["base_order_type"]
+        self.max_so_count = int(bot["max_so_count"])
+        self.price_deviation_so = bot["price_deviation_so"]
         self.division = self.balance / self.max_so_count + 2
-        self.take_profit = bot['take_profit']
-        self.trailling = bot['trailling']
-        self.trailling_deviation = bot['trailling_deviation']
+        self.take_profit = bot["take_profit"]
+        self.trailling = bot["trailling"]
+        self.trailling_deviation = bot["trailling_deviation"]
 
     def handle_fourofour(self, order):
-        if 'code' not in order:
+        if "code" not in order:
             # save base deal
             return order
         else:
@@ -55,58 +56,35 @@ class Deal:
     def long_base_order(self):
         # base_order = Buy_Order(symbol=self.symbol, quantity=self.division, type=self.base_order_type).post_order_limit()
         base_order = {
-            'clientOrderId': 'KxwRuUmnQqgcs5y7KWU77t', 
-            'cummulativeQuoteQty': '0.00000000', 
-            'executedQty': '0.00000000', 
-            'fills': [], 
-            'orderId': 263599681, 
-            'orderListId': -1, 
-            'origQty': '4.00000000', 
-            'price': '0.00039920', 
-            'side': 'BUY', 
-            'status': 'NEW', 
-            'symbol': 'EOSBTC', 
-            'timeInForce': 'GTC', 
-            'transactTime': 1574040139349, 
-            'type': 'LIMIT'
+            "clientOrderId": "KxwRuUmnQqgcs5y7KWU77t",
+            "cummulativeQuoteQty": "0.00000000",
+            "executedQty": "0.00000000",
+            "fills": [],
+            "orderId": 263599681,
+            "orderListId": -1,
+            "origQty": "4.00000000",
+            "price": "0.00039920",
+            "side": "BUY",
+            "status": "NEW",
+            "symbol": "EOSBTC",
+            "timeInForce": "GTC",
+            "transactTime": 1574040139349,
+            "type": "LIMIT",
         }
         base_deal = {
-            'order_id': base_order['orderId'],
-            'type': base_order['base_order'],
-            'strategy': 'long', # change accordingly
-            'pair': base_order['symbol'],
-            'order_side': base_order['side'],
-            'order_type': base_order['type'],
-            'price': base_order['price'],
-            'qty': base_order['origQty'],
-            'fills': base_order['fills'],
-            'time_in_force': base_order['timeInForce']
+            "order_id": base_order["orderId"],
+            "deal_type": "base_order",
+            "strategy": "long",  # change accordingly
+            "pair": base_order["symbol"],
+            "order_side": base_order["side"],
+            "order_type": base_order["type"],
+            "price": base_order["price"],
+            "qty": base_order["origQty"],
+            "fills": base_order["fills"],
+            "time_in_force": base_order["timeInForce"],
         }
-        self.base_order_price = base_order['price']
+        self.base_order_price = base_order["price"]
         return base_deal
-        
-
-    def long_take_profit_order(self):
-        price = self.division * self.max_so_count
-        order = Buy_Order(symbol=self.symbol, quantity=self.division, type='LIMIT', price=price).post_order_limit()
-        base_order = {
-            'deal_type': order['take_profit'],
-            'order_id': order['orderId'],
-            'type': order['base_order'],
-            'strategy': 'long', # change accordingly
-            'pair': order['symbol'],
-            'order_side': order['side'],
-            'order_type': order['type'],
-            'price': order['price'],
-            'qty': order['origQty'],
-            'fills': order['fills'],
-            'time_in_force': order['timeInForce']
-        }
-        if 'code' not in order:
-            return base_order
-        else:
-            print(order)
-            exit(1)
 
     def long_safety_order_generator(self):
         length = self.max_so_count
@@ -116,48 +94,95 @@ class Deal:
             price = self.division * index
             # order = Buy_Order(symbol=self.symbol, quantity=self.division, type='LIMIT', price=price).post_order_limit()
             order = {
-                'clientOrderId': 'KxwRuUmnQqgcs5y7KWU77t', 
-                'cummulativeQuoteQty': '0.00000000', 
-                'executedQty': '0.00000000', 
-                'fills': [], 
-                'orderId': 263599681, 
-                'orderListId': -1, 
-                'origQty': '4.00000000', 
-                'price': '0.00039920', 
-                'side': 'BUY', 
-                'status': 'NEW', 
-                'symbol': 'EOSBTC', 
-                'timeInForce': 'GTC', 
-                'transactTime': 1574040139349, 
-                'type': 'LIMIT'
+                "clientOrderId": "KxwRuUmnQqgcs5y7KWU77t",
+                "cummulativeQuoteQty": "0.00000000",
+                "executedQty": "0.00000000",
+                "fills": [],
+                "orderId": 263599681,
+                "orderListId": -1,
+                "origQty": "4.00000000",
+                "price": "0.00039920",
+                "side": "BUY",
+                "status": "NEW",
+                "symbol": "EOSBTC",
+                "timeInForce": "GTC",
+                "transactTime": 1574040139349,
+                "type": "LIMIT",
             }
             safety_orders = {
-                'order_id': order['orderId'],
-                'type': order['base_order'],
-                'strategy': 'long', # change accordingly
-                'pair': order['symbol'],
-                'order_side': order['side'],
-                'order_type': order['type'],
-                'price': order['price'],
-                'qty': order['origQty'],
-                'fills': order['fills'],
-                'time_in_force': order['timeInForce']
+                "order_id": order["orderId"],
+                "deal_type": "safety_order",
+                "strategy": "long",  # change accordingly
+                "pair": order["symbol"],
+                "order_side": order["side"],
+                "order_type": order["type"],
+                "price": price,
+                "qty": order["origQty"],
+                "fills": order["fills"],
+                "time_in_force": order["timeInForce"],
+                "so_count": index
             }
 
             so_deals.append(safety_orders)
-        return safety_orders
+        return so_deals
+
+    def long_take_profit_order(self):
+        price = self.division * self.max_so_count
+        # order = Buy_Order(symbol=self.symbol, quantity=self.division, type='LIMIT', price=price).post_order_limit()
+        # Make requests as with normal api
+        order = {
+            "clientOrderId": "KxwRuUmnQqgcs5y7KWU77t",
+            "cummulativeQuoteQty": "0.00000000",
+            "executedQty": "0.00000000",
+            "fills": [],
+            "orderId": 263599681,
+            "orderListId": -1,
+            "origQty": "4.00000000",
+            "price": "0.00039920",
+            "side": "BUY",
+            "status": "NEW",
+            "symbol": "EOSBTC",
+            "timeInForce": "GTC",
+            "transactTime": 1574040139349,
+            "type": "LIMIT",
+        }
+        base_order = {
+            "deal_type": "take_profit",
+            "order_id": order["orderId"],
+            "strategy": "long",  # change accordingly
+            "pair": order["symbol"],
+            "order_side": order["side"],
+            "order_type": order["type"],
+            "price": price,
+            "qty": order["origQty"],
+            "fills": order["fills"],
+            "time_in_force": order["timeInForce"],
+        }
+        if "code" not in order:
+            return base_order
+        else:
+            print(order)
+            exit(1)
 
     def open_deal(self):
-        print('opening deal')
-        new_deal = {
-            'base_order': {},
-            'take_profit_order': {},
-            'so_orders': []
-        }
-        new_deal['base_order'] = self.long_base_order()
-        new_deal['so_orders'] = self.long_safety_order_generator()
-        new_deal['take_profit_order'] = self.long_safety_order_generator()
-        
-        return 
+        new_deal = {"base_order": {}, "take_profit_order": {}, "so_orders": []}
+        long_base_order = self.long_base_order()
+        if not long_base_order:
+            print("Deal: Base order failed")
+        new_deal["base_order"] = long_base_order
 
+        long_safety_order_generator = self.long_safety_order_generator()
+        if not long_safety_order_generator:
+            print("Deal: Safety orders failed")
+        new_deal["so_orders"] = long_safety_order_generator
+
+        long_take_profit_order = self.long_take_profit_order()
+        if not long_take_profit_order:
+            print("Deal: Take profit order failed")
+
+        new_deal["take_profit_order"] = long_take_profit_order
+
+        dealId = app.db.deals.save(new_deal)
+        dealId = str(dealId)
+        return dealId
 
