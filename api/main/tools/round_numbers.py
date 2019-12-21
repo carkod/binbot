c21fa75bc2071c96ff465e04e608b5ca705bda47
Carlos Wu <carkodw@gmail.com>
2019-12-21 16:54:02 +0000

    round to ceiling util

--- api/main/tools/round_numbers.py
+++ api/main/tools/round_numbers.py
@@ -5,3 +5,10 @@ def round_numbers(value, decimals=6):
   number = float(value)
   result = math.floor(number * decimal_points) / decimal_points
   return result
+
+
+def round_numbers_ceiling(value, decimals=6):
+  decimal_points = 10 ** int(decimals)
+  number = float(value)
+  result = math.ceil(number * decimal_points) / decimal_points
+  return result
