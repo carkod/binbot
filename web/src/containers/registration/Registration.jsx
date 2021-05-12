0ba726a126d2b1e0a59d753d927736df439fad10
Carlos Wu Fei <carlos.fei@canonical.com>
2021-05-12 20:53:14 +0100

    Fix build URL issues

--- web/src/containers/registration/Registration.jsx
+++ web/src/containers/registration/Registration.jsx
@@ -4,12 +4,6 @@ import { Col, Row } from "reactstrap";
 import RegistrationForm from "../../components/RegistrationForm";
 import { connect } from "react-redux";
 import { registerUser } from "./actions";
-// const data = {
-//   userBgImg: 'https://picsum.photos/1440/900?random=1',
-//   userProfileImg: 'https://picsum.photos/300/300?random=1',
-//   username: 'carkod',
-//   userQuote: 'I like the way you work it'
-// }
 
 class Registration extends Component {
   handleSubmit = (data) => {
