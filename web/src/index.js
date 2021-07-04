bb49252e18c6fd810eba5916d7cd93d16fa9b228
Carlos Wu Fei <carlos.fei@canonical.com>
2021-07-04 03:29:23 +0200

    Cleanup and package updates

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Switch } from "react-router-dom";
 import App from "./App.jsx";
 import "./assets/scss/paper-dashboard.scss";
-import reportWebVitals from "./reportWebVitals";
 import configureStore from "./store";
 
 const store = configureStore({});
@@ -21,7 +20,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
