d9ce1b534c447bac61407fdc795d25330c9b52d1
Carlos Wu Fei <carlos.fei@canonical.com>
2021-10-05 15:24:20 +0100

    Test CI

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 import os
+import pprint
 from flask import Flask
 from flask_cors import CORS
-from pymongo import ASCENDING, MongoClient, errors
-from urllib.parse import quote_plus
+from pymongo import TEXT, MongoClient, errors
+import pprint
 
 def create_app():
     app = Flask(__name__)
@@ -14,18 +15,10 @@ def create_app():
     mongo = MongoClient(
         host=os.getenv("MONGO_HOSTNAME"),
         port=int(os.getenv("MONGO_PORT")),
-        # authSource=os.getenv("MONGO_AUTH_DATABASE"),
-        # username=os.getenv("MONGO_AUTH_USERNAME"),
-        # password=os.getenv("MONGO_AUTH_PASSWORD")
+        authSource="admin",
+        username=os.getenv("MONGO_AUTH_USERNAME"),
+        password=os.getenv("MONGO_AUTH_PASSWORD")
     )
     app.db = mongo[os.getenv("MONGO_APP_DATABASE")]
-    
-    # if "bots" not in app.db.list_collection_names():
-    #     app.db.create_collection("bots")
-
-    # # Set default indexes for db
-    # app.db.bots.create_index([
-    #     ("pair", ASCENDING)
-    # ])
     
     return app
