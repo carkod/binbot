70420c44892ec1fd264eca2f4ec81383c7c86a0f
Carlos Wu Fei <carlos.fei@canonical.com>
2021-12-18 10:09:55 +0000

    Fix new token signal repetition and timing

--- research/new_tokens.py
+++ research/new_tokens.py
@@ -6,7 +6,6 @@ import string
 import time
 import re
 from telegram_bot import TelegramBot
-from uniswap import Uniswap
 import os
 import json
 
@@ -50,23 +49,24 @@ class NewTokens(BinbotApi):
                 if t in self.last_processed_kline and (float(time.time()) - float(self.last_processed_kline[t])) > 86400:
                     del self.last_processed_kline[t]
 
-                get_date = latest_announcement['data']['catalogs'][0]['articles'][i]["releaseDate"]
-                dt_object = datetime.fromtimestamp(int(get_date / 1000))
-                release_date = dt_object.strftime("%Y-%m-%dT%H:%M")
-                if dt_object < datetime.now():
-                    headers={
-                        "User-Agent": "SomeAgent"
-                    }
-                    coin_data = get(url=f"https://etherscan.io/searchHandler?term={t}&filterby=0", headers=headers)
-                    json_data = json.loads(coin_data.text)
-                    for item in json_data:
-                        find_token = re.findall('\(([^)]+)', item)
-                        if len(find_token) > 0 and find_token[0] == t:
-                            # Get the address
-                            token_address = re.findall('0x[a-fA-F0-9]{40}', item)[0]
-                            msg = f"New token/cryptocurrency {t} about to launch {release_date}. Address: {token_address}"
-                            self.telegram_bot.send_msg(msg)
-                            print(msg)
-                    self.last_processed_kline[t] = time.time()
+                if t not in self.last_processed_kline:
+                    get_date = latest_announcement['data']['catalogs'][0]['articles'][i]["releaseDate"]
+                    dt_object = datetime.fromtimestamp(int(get_date / 1000))
+                    release_date = dt_object.strftime("%Y-%m-%dT%H:%M")
+                    if dt_object > datetime.now():
+                        headers={
+                            "User-Agent": "SomeAgent"
+                        }
+                        coin_data = get(url=f"https://etherscan.io/searchHandler?term={t}&filterby=0", headers=headers)
+                        json_data = json.loads(coin_data.text)
+                        for item in json_data:
+                            find_token = re.findall('\(([^)]+)', item)
+                            if len(find_token) > 0 and find_token[0] == t:
+                                # Get the address
+                                token_address = re.findall('0x[a-fA-F0-9]{40}', item)[0]
+                                msg = f"New token/cryptocurrency {t} about to launch {release_date}. Address: {token_address}"
+                                self.telegram_bot.send_msg(msg)
+                                print(msg)
+                        self.last_processed_kline[t] = time.time()
 
         pass
