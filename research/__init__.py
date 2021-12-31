07ec82120d067302b4409edbcb29b0e98360fd1b
Carlos Wu <carlos.fei@canonical.com>
2021-12-31 02:34:29 +0000

    Run new tokens check as cronjob

--- research/__init__.py
+++ research/__init__.py
@@ -1,18 +1,34 @@
-from signals import ResearchSignals
-from new_tokens import NewTokens
+import os
 import threading
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from new_tokens import NewTokens
+from signals import ResearchSignals
+import time
+
+if os.getenv("ENV") != "development" or os.getenv("ENV") != "ci":
+    scheduler = BackgroundScheduler()
+    nt = NewTokens()
+    scheduler.add_job(
+        func=nt.run,
+        timezone="Europe/London",
+        trigger="interval",
+        hours=3,
+    )
+    scheduler.start()
 
 if __name__ == "__main__":
-    # Research market updates
-    nt = NewTokens()
-    new_tokens_thread = threading.Thread(
-        name="new_tokens_thread", target=nt.run
-    )
-    new_tokens_thread.start()
-
     rs = ResearchSignals()
     rs_thread = threading.Thread(
         name="rs_thread", target=rs.start_stream
     )
     rs_thread.start()
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(5)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
