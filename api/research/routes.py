d3e0c092aaf8677e1590769faa775bffb9403015
Carlos Wu <carlos.fei@canonical.com>
2022-05-10 11:47:28 +0100

    Remove correlation data model and endpoints (#277)
    * Remove correlation data model and endpoints
    
    * Fix index error check_gaps
    
    * Fix create bot id

--- api/research/routes.py
+++ api/research/routes.py
@@ -1,38 +1,7 @@
 from flask import Blueprint
 from api.research.controller import Controller
-from api.research.correlation import Correlation
-from threading import Thread, enumerate
 
 research_blueprint = Blueprint("research", __name__)
-
-
-@research_blueprint.route("/save-correlation/<interval>/<limit>", methods=["GET"])
-def save_pearson(interval, limit):
-    correlation = Correlation()
-    match_thread = next(
-        (x for x in enumerate() if x.name == "save_correlation_thread"), None
-    )
-    if not match_thread:
-        thread = Thread(
-            name="save_correlation_thread",
-            target=correlation.trigger_r,
-            args=[interval, limit],
-        )
-        thread.start()
-        return correlation.response()
-    else:
-        return correlation.block_response()
-
-
-@research_blueprint.route("/signals", methods=["GET"])
-def get_signals():
-    return Correlation().get_signals()
-
-
-@research_blueprint.route("/historical-signals", methods=["GET"])
-def get_historical_signals():
-    return Correlation().get_historical_signals()
-
 
 @research_blueprint.route("/blacklist", methods=["POST"])
 def post_blacklist():
