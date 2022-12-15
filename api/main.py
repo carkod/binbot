5c556e7e58a4c6a11611d65fe2effee1c4e98414
Carlos Wu Fei <carkod@Carloss-MacBook-Pro.local>
2022-12-15 04:47:57 +0100

    Fix trailling top loss below buy_price

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import atexit
 import os
 import threading
+import uvicorn
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from api.account.assets import Assets
@@ -18,24 +19,25 @@ from api.research.routes import research_blueprint
 from api.user.routes import user_blueprint
 from api.paper_trading.routes import paper_trading_blueprint
 from api.autotrade.routes import autotrade_settings_blueprint
+from fastapi import BackgroundTasks, FastAPI
 
 
 app = create_app()
 
-@app.route("/")
+@app.get("/")
 def index():
     return jsonResp({"status": "Online"})
 
 
 # Register Blueprints
-app.register_blueprint(user_blueprint)
-app.register_blueprint(account_blueprint, url_prefix="/account")
-app.register_blueprint(bot_blueprint)
-app.register_blueprint(order_blueprint, url_prefix="/order")
-app.register_blueprint(charts_blueprint, url_prefix="/charts")
-app.register_blueprint(research_blueprint, url_prefix="/research")
-app.register_blueprint(paper_trading_blueprint)
-app.register_blueprint(autotrade_settings_blueprint, url_prefix="/autotrade-settings")
+# app.register_blueprint(user_blueprint)
+# app.register_blueprint(account_blueprint, url_prefix="/account")
+# app.register_blueprint(bot_blueprint)
+# app.register_blueprint(order_blueprint, url_prefix="/order")
+# app.register_blueprint(charts_blueprint, url_prefix="/charts")
+# app.register_blueprint(research_blueprint, url_prefix="/research")
+# app.register_blueprint(paper_trading_blueprint)
+# app.register_blueprint(autotrade_settings_blueprint, url_prefix="/autotrade-settings")
 
 if os.getenv("ENV") != "development" or os.getenv("ENV") != "ci":
     scheduler = BackgroundScheduler()
@@ -51,19 +53,27 @@ if os.getenv("ENV") != "development" or os.getenv("ENV") != "ci":
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown(wait=False))
 
-
-if os.getenv("ENV") != "ci":
+async def run_websockets():
     order_updates = OrderUpdates()
-    # start a worker process to move the received stream_data from the stream_buffer to a print function
-    worker_thread = threading.Thread(
-        name="order_updates_thread", target=order_updates.run_stream
-    )
-    worker_thread.start()
+    BackgroundTasks.add_task(order_updates.run_stream())
 
     # Research market updates
     market_updates = MarketUpdates()
-    market_updates_thread = threading.Thread(
-        name="market_updates_thread", target=market_updates.start_stream
-    )
-    market_updates_thread.start()
-    pass
+    BackgroundTasks.add_task(market_updates.start_stream())
+    yield
+
+if os.getenv("ENV") != "ci":
+    @app.on_event("startup")
+    async def startup_event():
+        OrderUpdates().run_stream()
+        MarketUpdates().start_stream()
+        return
+
+    # @app.on_event("shutdown")
+    # async def shutdown_event():
+    #     await global_listener.stop_listening()
+    #     return
+
+
+if __name__ == "__main__":
+    uvicorn.run("api.main:app", port=8006)
