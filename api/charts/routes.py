fda8c4cd0cf2b4cc8eda9dd0f48af85d76e0244d
Carlos Wu <carlos.fei@canonical.com>
2022-12-23 01:33:51 +0100

    Fastapi convert autotrade endpoints (#418)
    * Convert bot schemas to pydantic models
    
    * Account endpoints conversion to FastApi
    
    * Convert autotrade, research endpoints
    
    * Fix user, charts and paper_trading endpoints

--- api/charts/routes.py
+++ api/charts/routes.py
@@ -1,47 +1,37 @@
 from fastapi import APIRouter
 
-from api.charts.models import Candlestick, CandlestickItem
+from api.charts.models import Candlestick, CandlestickItemRequest
 
 charts_blueprint = APIRouter()
 
 
-@charts_blueprint.put("/candlestick", summary="Update klines stored in the DB")
-def update_klines(item: CandlestickItem):
+@charts_blueprint.put(
+    "/candlestick", summary="Update klines stored in the DB", tags=["charts"]
+)
+def update_klines(item: CandlestickItemRequest):
     """
-    json
-    {
-        "data": [[array]],
-        "symbol": string,
-        "interval: enum[string]
-        "limit": int (optional) default: 300
-        "offset": int (optional) default: 0
-    }
+    Update candlestick graph data in DB
     """
     return Candlestick().update_klines(item)
 
 
-@charts_blueprint.delete("/candlestick", summary="Delete Klines stored in the DB")
-def delete_klines(symbol: CandlestickItem):
+@charts_blueprint.delete(
+    "/candlestick", summary="Delete Klines stored in the DB", tags=["charts"]
+)
+def delete_klines(symbol):
     """
-    Query params
+    Delete candlestick graph data in DB by symbol.
 
-    symbol
+    This is used to update candlestick data that contains gaps and inconsistencies
     """
-    return Candlestick().delete_klines()
+    return Candlestick().delete_klines(symbol)
 
 
-@charts_blueprint.get("/candlestick", summary="Retrieved klines stored in DB")
-def get():
+@charts_blueprint.get(
+    "/candlestick", summary="Retrieved klines stored in DB", tags=["charts"]
+)
+def get(symbol: str, interval: str="15m", limit: int=500, start_time: float | None=None, end_time: float | None=None):
     """
-    @json:
-        {
-            "pair": string,
-            "interval": string,
-            "stats": boolean, Additional statistics such as MA, amplitude, volume, candle spreads
-            "binance": boolean, whether to directly pull data from binance or from DB
-            "limit": int,
-            "start_time": int
-            "end_time": int
-        }
+    Retrieve existing candlestick data stored in DB from Binance
     """
-    return Candlestick().get()
+    return Candlestick().get(symbol, interval, limit, start_time, end_time)
--- api/user/routes.py
+++ api/user/routes.py
@@ -2,40 +2,63 @@ from fastapi import APIRouter
 
 from api.auth import auth
 from api.user.models.user import User
+from api.user.schemas import UserSchema, LoginRequest, UserResponse
 
 user_blueprint = APIRouter()
 
 
-@user_blueprint.get("/user")
+@user_blueprint.get("/user", response_model=UserResponse, tags=["users"])
 def get():
+    """
+    Get all users
+    """
     return User().get()
 
 
-@user_blueprint.get("/user/<id>")
-def get_one(id):
-    return User().get_one()
+@user_blueprint.get("/user/{email}", response_model=UserResponse, tags=["users"])
+def get_one(email):
+    """
+    Get user by email
+    """
+    return User().get_one(email)
 
 
-@user_blueprint.post("/user/login")
-def login():
-    return User().login()
+@user_blueprint.post("/user/login", tags=["users"])
+def login(data: LoginRequest):
+    """
+    Get an access_token to keep the user in session
+    """
+    return User().login(data)
 
 
-@user_blueprint.get("/user/logout")
+@user_blueprint.get("/user/logout", tags=["users"])
 def logout():
+    """
+    Remove access_token
+    """
     return User().logout()
 
 
-@user_blueprint.post("/user/register")
-def add():
-    return User().add()
+@user_blueprint.post("/user/register", tags=["users"])
+def add(data: UserSchema):
+    """
+    Create/register a new user
+    """
+    return User().add(data)
 
 
-@user_blueprint.put("/user/<id>")
-def edit(id):
-    return User().edit()
+@user_blueprint.put("/user", tags=["users"])
+def edit(user: UserSchema):
+    """
+    Modify details of a user that already exists.
+    If the user does not exist, it will return a JSON error message
+    """
+    return User().edit(user)
 
 
-@user_blueprint.delete("/user/<id>")
-def delete(id):
-    return User().delete()
+@user_blueprint.delete("/user/{email}", tags=["users"])
+def delete(email: str):
+    """
+    Delete a user by email
+    """
+    return User().delete(email)
