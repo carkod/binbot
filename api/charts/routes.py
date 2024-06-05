54b1dda7b9eed202312127418293cee5e9bc0f44
Carlos Wu Fei <carkod@Carloss-MacBook-Pro.local>
2024-06-05 02:46:13 +0100

    Remove endpoints for old candlesticks

--- api/charts/routes.py
+++ api/charts/routes.py
@@ -4,39 +4,6 @@ from charts.models import Candlestick, CandlestickItemRequest
 
 charts_blueprint = APIRouter()
 
-
-@charts_blueprint.put(
-    "/candlestick", summary="Update klines stored in the DB", tags=["charts"]
-)
-def update_klines(item: CandlestickItemRequest):
-    """
-    Update candlestick graph data in DB
-    """
-    return Candlestick().update_klines(item)
-
-
-@charts_blueprint.delete(
-    "/candlestick", summary="Delete Klines stored in the DB", tags=["charts"]
-)
-def delete_klines(symbol):
-    """
-    Delete candlestick graph data in DB by symbol.
-
-    This is used to update candlestick data that contains gaps and inconsistencies
-    """
-    return Candlestick().delete_klines(symbol)
-
-
-@charts_blueprint.get(
-    "/candlestick", summary="Retrieved klines stored in DB", tags=["charts"]
-)
-def get(symbol: str, interval: str="15m", limit: int=500, start_time: float | None=None, end_time: float | None=None, stats: bool = False):
-    """
-    Retrieve existing candlestick data stored in DB from Binance
-    """
-    return Candlestick().get(symbol, interval, limit, start_time, end_time, stats)
-
-
 @charts_blueprint.get("/timeseries", summary="Retrieve timeseries data", tags=["charts"])
 def get_timeseries(symbol: str, limit: int=500):
     """
