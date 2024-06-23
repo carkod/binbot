35936a1c9b0271eb366f2a3160f00906ad83389a
Carlos Wu Fei <carkod@Carloss-MacBook-Pro.local>
2024-06-23 16:35:45 +0100

    Update bots with bb_spreads

---
+++ api/streaming/models.py
@@ -0,0 +1,31 @@
+
+from tools.enum_definitions import TrendEnum
+from pydantic import BaseModel, ConfigDict, field_validator
+
+class SignalsConsumer(BaseModel):
+    """
+    Pydantic model for the signals consumer.
+    """
+    type: str = "signal"
+    spread: float | str | None = 0
+    current_price: float | str | None = 0
+    msg: str
+    symbol: str
+    algo: str
+    trend: TrendEnum | None = TrendEnum.neutral
+
+    model_config = ConfigDict(
+        extra='allow',
+    )
+
+    @field_validator("spread", "current_price")
+    @classmethod
+    def name_must_contain_space(cls, v):
+        if v is None:
+            return 0
+        elif isinstance(v, str):
+            return float(v)
+        elif isinstance(v, float):
+            return v
+        else:
+            raise ValueError('must be a float or None')
