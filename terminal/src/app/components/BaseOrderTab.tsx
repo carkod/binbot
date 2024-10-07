f7f90494903d8c070e34fe4e4c5cedd9d5d7d5b4
Carlos Wu Fei <carkodw@gmail.com>
2024-10-08 00:33:39 +0100

    Main base order tabs field errors fixed

--- terminal/src/app/components/BaseOrderTab.tsx
+++ terminal/src/app/components/BaseOrderTab.tsx
@@ -1,5 +1,5 @@
 import { type FC, useEffect, useState } from "react"
-import { Badge, Col, Form, Row, Tab } from "react-bootstrap"
+import { Badge, Col, Container, Form, InputGroup, Row, Stack, Tab } from "react-bootstrap"
 import { useForm } from "react-hook-form"
 import { useAppDispatch } from "../hooks"
 import { type Bot, singleBot } from "../../features/bots/botInitialState"
@@ -7,7 +7,7 @@ import { type AppDispatch } from "../store"
 import { InputTooltip } from "./InputTooltip"
 import { setField } from "../../features/bots/botSlice"
 import { TabsKeys } from "../pages/BotDetail"
-import { BotStatus } from "../../utils/enums"
+import { BotStatus, BotStrategy } from "../../utils/enums"
 
 const BaseOrderTab: FC<{
   bot: Bot
@@ -62,9 +62,10 @@ const BaseOrderTab: FC<{
 
   return (
     <Tab.Pane id="main" eventKey={TabsKeys.MAIN} className="mb-3">
-      <Row>
-        <Col md="6" sm="12">
-          {/* <SymbolSearch
+      <Container>
+        <Row className="my-3">
+          <Col md="6" sm="12">
+            {/* <SymbolSearch
             name="Pair"
             label="Select pair"
             options={symbols}
@@ -74,127 +75,118 @@ const BaseOrderTab: FC<{
             disabled={bot.status === BotStatus.COMPLETED}
             required={true}
           /> */}
-        </Col>
-        <Col md="6" sm="12">
-          <Form.Label htmlFor="name">Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            onChange={handleChange}
-            value={bot.name}
-            {...register("name")}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md="6" sm="12">
-          <InputTooltip name="base_order_size" tooltip={"Minimum 15 USD"} title="Base order size">
+          </Col>
+          <Col md="6" sm="12">
+            <Form.Label htmlFor="name">Name</Form.Label>
             <Form.Control
               type="text"
-              name="base_order_size"
-              onChange={handleBaseChange}
-              onBlur={handleBlur}
-              value={bot.base_order_size}
-              autoComplete="off"
-              required
-              disabled={bot.status === BotStatus.ACTIVE || bot.status === BotStatus.COMPLETED}
+              name="name"
+              value={bot.name}
+              {...register("name")}
             />
-          </InputTooltip>
-          
-          {bot.status !== BotStatus.ACTIVE && (
-            <>
-              <Badge color="secondary" onClick={addMin}>
-                Min{" "}
-                {bot.quoteAsset === "BTC"
-                  ? 0.001
-                  : bot.quoteAsset === "BNB"
-                    ? 0.051
-                    : bot.quoteAsset === "USDC"
-                      ? 15
-                      : ""}
-              </Badge>{" "}
-              <Badge color="secondary" onClick={addAll}>
-                Add all
-              </Badge>
-            </>
-          )}
-        </Col>
-        {bot.status !== BotStatus.ACTIVE && (
-          <Col md="6" sm="12">
-            <Form.Label htmlFor="balance_to_use" aria-required="true">
-              Balance to use
-            </Form.Label>
-            <Form.Group
-              style={{
-                display: "flex",
-                alignItems: "center",
-                fontSize: "1.5rem",
-              }}
-            >
-              {bot.quoteAsset && (
-                <Form.Label>
-                  <Form.Control
-                    required
-                    type="radio"
-                    name="balance_to_use"
-                    checked={bot.balance_to_use === bot.quoteAsset}
-                    value={bot.quoteAsset}
-                    onChange={handleChange}
-                  />{" "}
-                  {bot.quoteAsset}
-                </Form.Label>
-              )}
-              <Form.Label>
-                <Form.Control
-                  type="radio"
-                  name="balance_to_use"
-                  checked={bot.balance_to_use === "GBP"}
-                  value={"GBP"}
-                  onChange={handleChange}
-                />{" "}
-                GBP
-              </Form.Label>
-            </Form.Group>
           </Col>
-        )}
-      </Row>
-      {/* <Row>
-        <Col md="6" sm="12">
-          <Form.Group>
+        </Row>
+        <Row className="my-3">
+          <Col md="6" sm="12">
+            <InputGroup>
+              <InputTooltip
+                name="base_order_size"
+                tooltip={"Minimum 15 USD"}
+                title="Base order size"
+                label="Base order size"
+                errors={errors}
+              >
+                <Form.Control
+                  type="text"
+                  name="base_order_size"
+                  onBlur={handleBlur}
+                  value={bot.base_order_size}
+                  autoComplete="off"
+                  required
+                  disabled={
+                    bot.status === BotStatus.ACTIVE ||
+                    bot.status === BotStatus.COMPLETED
+                  }
+                  {...register("base_order_size", {
+                    required: "Base order size is required",
+                    min: {
+                      value: 15,
+                      message: "Minimum base order size is 15",
+                    },
+                  })}
+                />
+              </InputTooltip>
+              {bot.quoteAsset && (
+                <InputGroup.Text>{bot.quoteAsset}</InputGroup.Text>
+              )}
+            </InputGroup>
+
+            {bot.status !== BotStatus.ACTIVE && (
+              <>
+                <Badge color="secondary" onClick={addMin}>
+                  Min{" "}
+                  {bot.quoteAsset === "BTC"
+                    ? 0.001
+                    : bot.quoteAsset === "BNB"
+                      ? 0.051
+                      : bot.quoteAsset === "USDC"
+                        ? 15
+                        : ""}
+                </Badge>{" "}
+                <Badge color="secondary" onClick={addAll}>
+                  Add all
+                </Badge>
+              </>
+            )}
+          </Col>
+          <Col md="6" sm="12">
+            <Stack direction="horizontal">
+              <Badge>{bot.balance_to_use}</Badge>
+            </Stack>
+          </Col>
+        </Row>
+        <Row className="my-3">
+          <Col md="6" sm="12">
             <InputTooltip
               name="cooldown"
-              text="Time until next bot activation with same pair"
+              tooltip="Time until next bot activation with same pair"
+              label="Cooldown (seconds)"
+              title="Cooldown"
+              errors={errors}
+              {...register("cooldown")}
             >
-              Cooldown (seconds)
+              <Form.Control
+                type="number"
+                name="cooldown"
+                value={bot.cooldown}
+                autoComplete="off"
+              />
             </InputTooltip>
-            <Form.Control
-              type="number"
-              name="cooldown"
-              onChange={handleChange}
-              value={bot.cooldown}
-              autoComplete="off"
-            />
-          </Form.Group>
-        </Col>
-      </Row> */}
-      {/* <Row>
-        <Col md="6" sm="12">
-          <Form.Group>
-            <Form.Label htmlFor="strategy">Trigger strategy</Form.Label>
-            <Form.Control
-              id="strategy"
-              name="strategy"
-              type="select"
-              value={bot.strategy}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            >
-              <option value="long">Long</option>
-              <option value="margin_short">Margin short</option>
-            </Form.Control>
-          </Form.Group>
-        </Col>
-      </Row> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col md="6" sm="12">
+            <Form.Group>
+              <Form.Label htmlFor="strategy">Trigger strategy</Form.Label>
+              <Form.Select
+                id="strategy"
+                name="strategy"
+                value={bot.strategy}
+                onBlur={handleBlur}
+                {...register("strategy", { required: "Strategy is required" })}
+              >
+                <option value={BotStrategy.LONG}>Long</option>
+                <option value={BotStrategy.MARGIN_SHORT}>Margin short</option>
+              </Form.Select>
+              {errors.strategy && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.strategy.message}
+                </Form.Control.Feedback>
+              )}
+            </Form.Group>
+          </Col>
+        </Row>
+      </Container>
     </Tab.Pane>
   )
 }
--- terminal/src/app/components/InputTooltip.tsx
+++ terminal/src/app/components/InputTooltip.tsx
@@ -1,26 +1,33 @@
 import { type FC } from "react"
-import { Button, Form } from "react-bootstrap"
-import { Label } from "reactstrap"
+import { Form } from "react-bootstrap"
+import { type FieldErrors } from "react-hook-form"
+import { type Bot } from "../../features/bots/botInitialState"
 
 interface InputTooltipProps {
   name: string
   tooltip: string
   children: React.ReactNode
   title: string
-
+  label: string
+  errors: FieldErrors<Bot>
 }
 
-export const InputTooltip: FC<InputTooltipProps> = ({ name, tooltip, children }, ...props) => {
+export const InputTooltip: FC<InputTooltipProps> = (
+  { name, tooltip, label, children, errors },
+  ...props
+) => {
   return (
     <Form.Group className="position-relative">
-      <Form.Label htmlFor={name}>
-        <Button type="button" className="btn--tooltip" id={`${name}-tooltip`}>
-          {children}
-        </Button>
-      </Form.Label>
+      <Form.Label htmlFor={name}>{label}</Form.Label>
+      {children}
       <Form.Control.Feedback>
         <small>{tooltip}</small>
       </Form.Control.Feedback>
+      {errors[name] && (
+        <Form.Control.Feedback type="invalid">
+          {errors[name as keyof FieldErrors].message}
+        </Form.Control.Feedback>
+      )}
     </Form.Group>
   )
 }
