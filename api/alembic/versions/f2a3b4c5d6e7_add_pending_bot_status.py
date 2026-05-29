c8fd102e13ba4c1d044a26c8af54a5517fb81c1b
Carlos Wu <carlos.wufei@houseful.co.uk>
2026-05-29 18:15:29 +0100

    Fix pending status migration

--- api/alembic/versions/f2a3b4c5d6e7_add_pending_bot_status.py
+++ api/alembic/versions/f2a3b4c5d6e7_add_pending_bot_status.py
@@ -1,6 +1,6 @@
 """add pending bot status
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: f2a3b4c5d6e7
 Revises: e2f3a4b5c6d7
 Create Date: 2026-05-26 00:00:00.000000
 
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 
 from alembic import op
 
-revision: str = "a1b2c3d4e5f6"
+revision: str = "f2a3b4c5d6e7"
 down_revision: Union[str, Sequence[str], None] = "e2f3a4b5c6d7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
