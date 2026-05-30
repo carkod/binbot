90312766fc54516d1c708c0820f4d5cd4fb030f4
carkod <carkodw@gmail.com>
2026-05-30 19:21:26 +0100

    Refactor and separate exchange-specific proxy endpoints

--- api/orders/routes/binance.py
+++ api/orders/routes/binance.py
@@ -1,27 +1,27 @@
+from exchange_apis.binance.orders import BinanceOrderController
 from fastapi import APIRouter, Depends
-from pybinbot import KucoinApi, BinanceErrors
+from orders.schemas import OrderParams
+from pybinbot import BinanceErrors
+from tools.config import Config
+from tools.handle_error import json_response, json_response_error
 from user.models.user import UserTokenData
 from user.services.auth import get_current_user
-from tools.handle_error import json_response, json_response_error
-from exchange_apis.binance.orders import BinanceOrderController
-from orders.schemas import OrderParams
-from tools.config import Config
 
 config = Config()
-order_blueprint = APIRouter()
+binance_order_blueprint = APIRouter()
 
 
-@order_blueprint.post("/buy", tags=["orders"])
+@binance_order_blueprint.post("/buy", tags=["orders"])
 def create_buy_order(item: OrderParams, _: UserTokenData = Depends(get_current_user)):
     return BinanceOrderController().buy_order(symbol=item.pair, qty=item.qty)
 
 
-@order_blueprint.post("/sell", tags=["orders"])
+@binance_order_blueprint.post("/sell", tags=["orders"])
 def create_sell_order(item: OrderParams, _: UserTokenData = Depends(get_current_user)):
     return BinanceOrderController().sell_order(symbol=item.pair, qty=item.qty)
 
 
-@order_blueprint.delete("/close/{symbol}/{orderid}", tags=["orders"])
+@binance_order_blueprint.delete("/close/{symbol}/{orderid}", tags=["orders"])
 def delete_order(symbol, orderid, _: UserTokenData = Depends(get_current_user)):
     try:
         data = BinanceOrderController().delete_order(symbol, orderid)
@@ -33,12 +33,12 @@ def delete_order(symbol, orderid, _: UserTokenData = Depends(get_current_user)):
     return resp
 
 
-@order_blueprint.get("/margin/sell/{symbol}/{qty}", tags=["orders"])
+@binance_order_blueprint.get("/margin/sell/{symbol}/{qty}", tags=["orders"])
 def margin_sell(symbol, qty, _: UserTokenData = Depends(get_current_user)):
     return BinanceOrderController().sell_margin_order(symbol, qty)
 
 
-@order_blueprint.get("/all-orders", tags=["orders"])
+@binance_order_blueprint.get("/all-orders", tags=["orders"])
 def get_all_orders(
     symbol,
     order_id: str | None = None,
@@ -50,26 +50,6 @@ def get_all_orders(
             symbol, order_id=order_id, start_time=start_time
         )
         return json_response({"message": "Orders found!", "data": data})
-    except ValueError as error:
-        return json_response_error(error.args[0])
-    except BinanceErrors as error:
-        return json_response_error(error.message)
-
-
-@order_blueprint.get("/kucoin/{symbol}/{order_id}", tags=["orders"])
-def get_order_by_id(
-    symbol: str, order_id: str, _: UserTokenData = Depends(get_current_user)
-):
-    try:
-        data = KucoinApi(
-            key=config.kucoin_key,
-            secret=config.kucoin_secret,
-            passphrase=config.kucoin_passphrase,
-        ).get_order(symbol=symbol, order_id=order_id)
-        return {
-            "message": "Order found!",
-            "data": data,
-        }
     except ValueError as error:
         return json_response_error(error.args[0])
     except BinanceErrors as error:
